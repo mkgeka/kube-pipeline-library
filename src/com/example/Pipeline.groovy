1621cad0fae8eb00783f35d06a6ef503fa3c30f0
mkgeka <mkgeka@gmail.com>
2020-10-05 17:23:28 +0200

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -11,9 +11,10 @@ class Pipeline {
     def execute() {
 	    script.node("master") {
 		script.git("https://github.com/Brialius/test-maven-project.git")
-		script.sh("cd project && mvn clean test")
 		script.stage("read")
-		script.stage("Build")
+		    script.stage("Build") {
+			    script.sh("cd project && mvn clean test")
+		    }
 	    	script.stage("Deploy")
 	    }
     }
