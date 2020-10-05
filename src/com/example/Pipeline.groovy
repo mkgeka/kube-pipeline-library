28313c817ffc8b3000fbfe0427a47200fa5cdbe4
mkgeka <mkgeka@gmail.com>
2020-10-05 16:26:53 +0200

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -8,10 +8,17 @@ class Pipeline {
         this.script = script
         this.configurationFile = configurationFile
     }
+    def checkOutFrom(repo) {
+  		git url: "https://github.com/mkgeka/test-maven-project.git"
+	}
 
+return this
     def execute() {
 	    script.node("master") {
-		script.stage("source")
+		    script.stage("source") {
+			def z = new org.foo.Zot()
+			z.checkOutFrom(repo)  
+		    }
 		script.stage("Build")
 	    	script.stage("Deploy")
 	    }
