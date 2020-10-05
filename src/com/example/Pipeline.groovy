f41768aa23414392e68a1058357113183423f59d
mkgeka <mkgeka@gmail.com>
2020-10-05 14:58:03 +0200

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -10,6 +10,9 @@ class Pipeline {
     }
 
     def execute() {
+	    script.stage("source") {
+		    git url: git url: 'https://github.com/Brialius/test-maven-project.git'
+	    }
 	    script.stage("test") 
 	    script.stage("Build")
 	    script.stage("Deploy")
