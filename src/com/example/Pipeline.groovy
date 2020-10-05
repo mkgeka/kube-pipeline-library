d7a2884e5d9c8ef69cf664ed089447eafe3f12fb
mkgeka <mkgeka@gmail.com>
2020-10-05 18:43:26 +0200

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -14,7 +14,7 @@ class Pipeline {
 		def data = script.readFile(configurationFile)
 		script.stage("notifications")
 		    script.stage("build") {
-			    println(data)
+			    println(configurationFile)
 		    }
 		script.stage("database")
 		script.stage("deploy")
