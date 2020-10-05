c33318a3bcb1044b33025abbfa1a98cb7eec734c
mkgeka <mkgeka@gmail.com>
2020-10-05 14:26:39 +0200

    Update Zot.groovy

--- src/com/example/Zot.groovy
+++ src/com/example/Zot.groovy
@@ -1,7 +1,6 @@
 package com.example
-
-def checkOutFrom() {
-  git url: "https://github.com/mkgeka/test-maven-project.git"
+class Zot() {
+  def checkOutFrom() {
+    git url: "https://github.com/mkgeka/test-maven-project.git"
+  }
 }
-
-return this
