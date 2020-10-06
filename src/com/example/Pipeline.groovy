1574b72b02adfdca00c57197ae74394916b98ebd
mkgeka <mkgeka@gmail.com>
2020-10-06 17:08:26 +0200

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -13,7 +13,7 @@ class Pipeline {
 	    script.node("master") {
 		script.git("https://github.com/mkgeka/test-maven-project.git")
 		def valuesYaml = script.readYaml(file: configurationFile)
-		    script.stage("Initialize") { valuesYaml.getClass() }
+		    script.stage("Initialize") { println valuesYaml.getClass() }
 		    script.stage("notifications") { valuesYaml.notifications }
 		    script.stage("build") { script.step(valuesYaml.build) }
 		    script.stage("database") { valuesYaml.database }
