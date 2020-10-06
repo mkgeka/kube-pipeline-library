5657e82306d275250910eee6bb17d5467f41ca93
mkgeka <mkgeka@gmail.com>
2020-10-06 20:02:40 +0200

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -16,7 +16,7 @@ class Pipeline {
 		    //script.stage("notifications") { script.step([$class: valuesYaml.notifications]) }
 		    //script.stage("database") { script.step(valuesYaml.database) }
 		    //script.stage("test") { script.step(valuesYaml.test) }
-		    script.stage("build") { script.step([$class: valuesYaml.build]) }
+		    script.stage("build") { script.step() { [$class: valuesYaml.build] } }
 	    }
     }
 }
