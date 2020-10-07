1173d810c115756fa5f9a1388c9d010ed7b79794
mkgeka <mkgeka@gmail.com>
2020-10-07 18:06:21 +0200

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -40,7 +40,7 @@ class Pipeline {
             }
 		    catch(all) {
 			    def recipients = valuesYaml.notifications.email.recipients
-			    script.println env.STAGE_NAME" 
+			    script.println env.STAGE_NAME 
 		    }
 	    }
     }
