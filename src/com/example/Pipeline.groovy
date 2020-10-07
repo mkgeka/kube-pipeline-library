bfb6a566f230751cb953a759ec0258b8e57ae611
mkgeka <mkgeka@gmail.com>
2020-10-07 14:55:36 +0200

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -33,7 +33,8 @@ class Pipeline {
 			    def testFolder = valuesYaml.test.testFolder
 			    def name = [ valuesYaml.test.name ]
 			    def testCommand = [ valuesYaml.test.testCommand ]
-			    script.echo name[0]
+			    for (itemName in name) { script.echo itemName }
+			    for (itemCommand in testCommand) { script.echo itemCommand }
 			    //script.sh "cd ${projectFolder} && ${deployCommand}" 
 		    }
 	    }
