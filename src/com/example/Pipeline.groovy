1fc111169deb94f95b450315302b9a125312bb9e
mkgeka <mkgeka@gmail.com>
2020-10-08 10:38:11 +0200

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -36,7 +36,7 @@ class Pipeline {
 			    def testCommand = valuesYaml.test.testCommand
 			    def arrayLength = name.size()
 			    for (i = 0; i <arrayLength; i++) { ptask["${i}"] = { script.dir(testFolder[i]) { script.sh "${testCommand[i]}" } } }
-			    script.parallel ptask
+			    parallel ptask
 		    }
             }
 		    catch(all) {
