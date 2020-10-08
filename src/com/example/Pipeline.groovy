60779876fbb479858b599c8d452d176979ee246a
mkgeka <mkgeka@gmail.com>
2020-10-08 11:24:24 +0200

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -26,21 +26,39 @@ class Pipeline {
 			    }
 		    }
 		    script.stage(stage[1]) {
+			    try {
 			    def databaseFolder = valuesYaml.database.databaseFolder
 			    def databaseCommand = valuesYaml.database.databaseCommand
 			    script.dir(databaseFolder) { script.sh "${databaseCommand}" } 
+			    }
+			    catch(all) {
+			    	def recipients = valuesYaml.notifications.email.recipients
+			    	script.println "The stage ${stage[1]} has been failed so sending email to ${recipients}"
+			    }
 		    }
 		    script.stage(stage[2]) {
+			    try {
 			    def projectFolder = valuesYaml.build.projectFolder
 			    def deployCommand = valuesYaml.deploy.deployCommand
 			    script.dir(projectFolder) { script.sh "${deployCommand}" }
+			    }
+			    catch(all) {
+			    	def recipients = valuesYaml.notifications.email.recipients
+			    	script.println "The stage ${stage[2]} has been failed so sending email to ${recipients}"
+			    }
 		    }
 		    script.stage(stage[3]) {
+			    try {
 			    def testFolder = valuesYaml.test.testFolder
 			    def name = valuesYaml.test.name
 			    def testCommand = valuesYaml.test.testCommand
 			    def arrayLength = name.size()
 			    for (def i = 0; i <arrayLength; i++) { script.dir(testFolder[i]) { script.sh "${testCommand[i]}" } }
+			    }
+			    catch(all) {
+			    	def recipients = valuesYaml.notifications.email.recipients
+			    	script.println "The stage ${stage[3]} has been failed so sending email to ${recipients}"
+			    }
 		    }
             }
     }
