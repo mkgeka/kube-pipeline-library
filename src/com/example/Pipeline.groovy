3a08e3b97b9bb038a61cdffed133d10576aee377
mkgeka <mkgeka@gmail.com>
2020-10-08 11:35:25 +0200

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -11,54 +11,36 @@ class Pipeline {
     }
     def execute() {
 	    script.node("master") {
-		    script.git("https://github.com/mkgeka/test-maven-project.git")
-		    def valuesYaml = script.readYaml(file: configurationFile)
-		    def stage = [ 'build', 'database', 'deploy', 'test']
-		    script.stage(stage[0]) {
-			    try {
-			    def projectFolder = valuesYaml.build.projectFolder
-			    def buildCommand = valuesYaml.build.buildCommand
-			    script.dir(projectFolder) { script.sh "${buildCommand}" }
+		    try {
+			    script.git("https://github.com/mkgeka/test-maven-project.git")
+			    def valuesYaml = script.readYaml(file: configurationFile)
+			    def stage = [ 'build', 'database', 'deploy', 'test']
+			    script.stage(stage[0]) {
+				    def projectFolder = valuesYaml.build.projectFolder
+				    def buildCommand = valuesYaml.build.buildCommand
+				    script.dir(projectFolder) { script.sh "${buildCommand}" }
 			    }
-			    catch(all) {
-			    	def recipients = valuesYaml.notifications.email.recipients
-			    	script.println "The stage ${stage[0]} has been failed so sending email to ${recipients}"
+			    script.stage(stage[1]) {
+				    def databaseFolder = valuesYaml.database.databaseFolder
+				    def databaseCommand = valuesYaml.database.databaseCommand
+				    script.dir(databaseFolder) { script.sh "${databaseCommand}" } 
 			    }
-		    }
-		    script.stage(stage[1]) {
-			    try {
-			    def databaseFolder = valuesYaml.database.databaseFolder
-			    def databaseCommand = valuesYaml.database.databaseCommand
-			    script.dir(databaseFolder) { script.sh "${databaseCommand}" } 
+			    script.stage(stage[2]) {
+				    def projectFolder = valuesYaml.build.projectFolder
+				    def deployCommand = valuesYaml.deploy.deployCommand
+				    script.dir(projectFolder) { script.sh "${deployCommand}" }
 			    }
-			    catch(all) {
-			    	def recipients = valuesYaml.notifications.email.recipients
-			    	script.println "The stage ${stage[1]} has been failed so sending email to ${recipients}"
-			    }
-		    }
-		    script.stage(stage[2]) {
-			    try {
-			    def projectFolder = valuesYaml.build.projectFolder
-			    def deployCommand = valuesYaml.deploy.deployCommand
-			    script.dir(projectFolder) { script.sh "${deployCommand}" }
-			    }
-			    catch(all) {
-			    	def recipients = valuesYaml.notifications.email.recipients
-			    	script.println "The stage ${stage[2]} has been failed so sending email to ${recipients}"
+			    script.stage(stage[3]) {
+				    def testFolder = valuesYaml.test.testFolder
+				    def name = valuesYaml.test.name
+				    def testCommand = valuesYaml.test.testCommand
+				    def arrayLength = name.size()
+				    for (def i = 0; i <arrayLength; i++) { script.dir(testFolder[i]) { script.sh "${testCommand[i]}" } }
 			    }
 		    }
-		    script.stage(stage[3]) {
-			    try {
-			    def testFolder = valuesYaml.test.testFolder
-			    def name = valuesYaml.test.name
-			    def testCommand = valuesYaml.test.testCommand
-			    def arrayLength = name.size()
-			    for (def i = 0; i <arrayLength; i++) { script.dir(testFolder[i]) { script.sh "${testCommand[i]}" } }
-			    }
-			    catch(all) {
+		    catch(all) {
 			    	def recipients = valuesYaml.notifications.email.recipients
 			    	script.println "The stage ${stage[3]} has been failed so sending email to ${recipients}"
-			    }
 		    }
 	    }
     }
