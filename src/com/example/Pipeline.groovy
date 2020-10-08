cee0bf02015373cbfe99b37869a08c89a1b751bd
mkgeka <mkgeka@gmail.com>
2020-10-08 11:10:30 +0200

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -16,24 +16,24 @@ class Pipeline {
 		    def stage = [ 'build', 'database', 'deploy', 'test']
 		    try {
 		    script.stage(stage[0]) {
-			    def projectFolder = valuesYaml.stage[0].projectFolder
+			    def projectFolder = valuesYaml.stage\[0\].projectFolder
 			    def buildCommand = valuesYaml.stage[0].buildCommand
 			    script.dir(projectFolder) { script.sh "${buildCommand}" }
 		    }
 		    script.stage(stage[1]) {
-			    def databaseFolder = valuesYaml.stage[1].databaseFolder
+			    def databaseFolder = valuesYaml.stage\[1\].databaseFolder
 			    def databaseCommand = valuesYaml.stage[1].databaseCommand
 			    script.dir(databaseFolder) { script.sh "${databaseCommand}" } 
 		    }
 		    script.stage(stage[2]) {
-			    def projectFolder = valuesYaml.stage[0].projectFolder
-			    def deployCommand = valuesYaml.stage[2].deployCommand
+			    def projectFolder = valuesYaml.stage\[0\].projectFolder
+			    def deployCommand = valuesYaml.stage\[2\].deployCommand
 			    script.dir(projectFolder) { script.sh "${deployCommand}" }
 		    }
 		    script.stage(stage[3]) {
-			    def testFolder = valuesYaml.stage[3].testFolder
-			    def name = valuesYaml.stage[3].name
-			    def testCommand = valuesYaml.stage[3].testCommand
+			    def testFolder = valuesYaml.stage\[3\].testFolder
+			    def name = valuesYaml.stage\[3\].name
+			    def testCommand = valuesYaml.stage\[3\].testCommand
 			    def arrayLength = name.size()
 			    for (def i = 0; i <arrayLength; i++) { script.dir(testFolder[i]) { script.sh "${testCommand[i]}" } }
 		    }
