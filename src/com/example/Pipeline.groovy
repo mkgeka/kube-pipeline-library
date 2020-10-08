8c21d9de2be6ec3c726ec3c861cc49520e37265c
mkgeka <mkgeka@gmail.com>
2020-10-08 17:35:37 +0200

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -42,7 +42,7 @@ class Pipeline {
 				    def testCommand = valuesYaml.test.testCommand
 				    def arrayLength = name.size()
 				    def builders = [:]
-				    for (def i = 0; i <arrayLength; i++) { builders["test"] = { script.dir(testFolder[i]) { script.sh "${testCommand[i]}" } } }
+				    for (def i = 0; i <arrayLength; i++) { builders[i] = { script.dir(testFolder[i]) { script.sh "${testCommand[i]}" } } }
 				    script.parallel builders
 			    }
 		    }
