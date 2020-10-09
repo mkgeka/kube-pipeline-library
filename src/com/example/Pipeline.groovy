5ba500ba6e37f339045b42ee99a880b6ba7a5dbe
mkgeka <mkgeka@gmail.com>
2020-10-09 11:29:50 +0200

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -17,7 +17,7 @@ class Pipeline {
 		    def stage = [ 'build', 'database', 'deploy', 'test']
 		    try {
 			    script.stage(stage[0]) {
-				    script.environment  { STAGE_NAME = "123" }
+				    script.environment  { FOO = "123" }
 				    script.println "Current running ${stage[0]}"
 				    def projectFolder = valuesYaml.build.projectFolder
 				    def buildCommand = valuesYaml.build.buildCommand
@@ -30,14 +30,14 @@ class Pipeline {
 				    script.dir(databaseFolder) { script.sh "${databaseCommand}" } 
 			    }
 			    script.stage(stage[2]) {
-				    script.environment  { STAGE_NAME = "123" }
+				    script.environment  { FOO = "123" }
 				    script.println "Current running ${stage[2]}"
 				    def projectFolder = valuesYaml.build.projectFolder
 				    def deployCommand = valuesYaml.deploy.deployCommand
 				    script.dir(projectFolder) { script.sh "${deployCommand}" }
 			    }
 			    script.stage(stage[3]) {
-				    script.environment  { STAGE_NAME = "123" }
+				    script.environment  { FOO = "123" }
 				    script.println "Current running ${stage[3]}"
 				    def testFolder = valuesYaml.test.testFolder
 				    def name = valuesYaml.test.name
